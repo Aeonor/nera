4f3c30d6ac1f8d08498296d188c58fe1289a2912
Aeonor <aeonor@gmail.com>
2014-05-25 21:40:09 +0200

    styles

--- l-header.php
+++ l-header.php
@@ -1,26 +1,26 @@
 <header class="l-header">
-    <div class="l-container">
+    <div class="r-container">
         <a href="#" class="l-header-logo">
             <img src="images/neralind-logo.png" alt="Neralind.com - Communauté d'esprit" />
         </a>
-        <div class="l-menu">
-            <nav>
-                <ul class="l-menu-list icon-list">
-                    <li class="icon icon-square-menu"><a href="#">Menu</a></li>
-                    <li class="icon icon-square-tags"><a href="#">Les tags</a></li>
-                    <li class="icon icon-square-contact"><a href="#">Contact</a></li>
-                    <li class="icon icon-square-fb"><a href="#">Facebook</a></li>
-                    <li class="icon icon-square-twitter"><a href="#">Twitter</a></li>
-                    <li class="icon icon-square-help"><a href="#">Aide</a></li>
-                </ul>
-            </nav>
 
-            <form method="get" action="#" class="search">
-                <div>
-                    <input type="search" name="q" placeholder="Rechercher sur le site" />
-                    <input type="submit" class="btn btn-search" value="Search" />
-                </div>
-            </form>
-        </div>
+        <nav class="l-header-menu">
+            <ul class="l-header-menu-list icon-list">
+                <li class="l-header-menu-item l-header-menu-item-menu"><a href="#">Menu</a></li>
+                <li class="l-header-menu-item l-header-menu-item-tags"><a href="#">Les tags</a></li>
+                <li class="l-header-menu-item"><a href="#">Contact</a></li>
+                <li class="l-header-menu-item"><a href="#">Facebook</a></li>
+                <li class="l-header-menu-item"><a href="#">Twitter</a></li>
+                <li class="l-header-menu-item"><a href="#">Aide</a></li>
+            </ul>
+        </nav>
+
+        <form method="get" action="#" class="l-header-form form-search">
+            <div>
+                <input type="search" name="q" placeholder="Rechercher sur le site" />
+                <input type="submit" class="btn btn-light btn-search" value="Search" />
+            </div>
+        </form>
+        
     </div>
 </header>
